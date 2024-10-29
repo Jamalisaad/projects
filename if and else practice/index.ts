32d2b3fe7ae06d7672563073a6c2e17add2748e7
Jamalisaad <jamalisaad377@gmail.com>
2024-10-29 19:38:13 +0500

    first commit

---
+++ if and else practice/index.ts
@@ -0,0 +1,59 @@
+// let x = 10;
+// let y =20;
+
+// console.log(true && true);
+// console.log(true && false);
+// console.log(false && true);
+// console.log(false && false);
+
+// console.log(x === 10 && y ===20);
+// console.log(x===10 && y === 30);
+// console.log(x<20 && y>10);
+
+
+// let a = 25;
+// let b = 30;
+// console.log(true || true);
+// console.log(true || false);
+// console.log(false || true);
+// console.log(false || false);
+
+// console.log(a ===25 || b === 20);
+// console.log(a===30 || b===35);
+// console.log(a===20 || b === 25);
+// console.log(a!==30);
+
+
+
+//=================conditional statement================
+// let personAge = 17;
+
+// if(personAge>18){
+//     console.log("he can vote")
+// }else{console.log("you can`t vote please sit on home")}
+
+
+
+// let temprature = 20;
+// if(temprature>30)
+//     {
+//     console.log("weather is hot");
+// }else{console.log("the weather is cold")}
+
+
+let temprature =42;
+
+if(temprature>30){
+    console.log("weather is hot");
+}else if(temprature>40){
+    console.log("the weather is vrey hot");
+    
+}else{
+    console.log("the weather is cold");
+}
+
+
+
+
+
+
---
+++ main.js
@@ -0,0 +1,27 @@
+var Marks = 45;
+Marks = 70;
+if (Marks === 45) {
+    console.log("he passed");
+}
+else if (Marks > 60) {
+    console.log("he got second position");
+}
+else if (Marks < 40) {
+    console.log("try again");
+}
+else {
+    console.log("he failed");
+}
+var temprature = 30;
+temprature = 31;
+if (temprature > 35) {
+    console.log('weather is very hot');
+}
+else if (temprature = 25) {
+    console.log('normal weather');
+}
+else if (temprature > 30) {
+    console.log('feeling hot');
+}
+else
+    (console.log)('weather is cold');
